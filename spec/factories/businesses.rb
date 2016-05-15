b5de2da627e82eb87f6e0d76e10f54dcdb54e113
jd3t23 <justinellis833@gmail>
2016-05-15 16:30:07 -0400

    adds api scaffold for business

---
+++ spec/factories/businesses.rb
@@ -0,0 +1,15 @@
+FactoryGirl.define do
+  factory :business do
+    uuid "MyString"
+name "MyString"
+address "MyString"
+address2 "MyString"
+city "MyString"
+state "MyString"
+zip "MyString"
+country "MyString"
+phone "MyString"
+website "MyString"
+  end
+
+end

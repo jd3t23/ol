676341e8c9e5e9a77141d50bb50401610f0f954b
jd3t23 <justinellis833@gmail>
2016-05-15 16:58:04 -0400

    adds seed data, import

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+  business_data_file_path = 'db/fixtures/50k_businesses.csv'
+  CSV.foreach(business_data_file_path, headers: true) do |row|
+    Business.create! row.to_hash
+  end

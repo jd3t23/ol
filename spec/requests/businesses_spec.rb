5d4199325c561b111dd4b4a88c599461eb928c83
jd3t23 <justinellis833@gmail>
2016-05-15 21:57:52 -0400

    json response includes all columns from the csv seed data

--- spec/requests/businesses_spec.rb
+++ spec/requests/businesses_spec.rb
@@ -13,21 +13,36 @@ RSpec.describe "Businesses", type: :request do
       expect(response).to be_success
       expect(response.content_type).to eq("application/json")
     end
+
+    it 'renders the json response containing all columns from the csv' do
+      column_headers = 'id,uuid,name,address,address2,city,state,zip,country,phone,website,created_at'.split(',')
+      JSON.parse(response.body).each { |json_response| expect(column_headers-json_response.keys).to be_empty }
+    end
   end
 
   describe "GET #show" do
 
-    before(:each) do
-      @business = FactoryGirl.create(:business)
-    end
+    context 'on successful request' do
 
-    it "returns json with successful status (200), when sending existing business id" do
-      get business_path(id: @business.id)
-      expect(response).to be_success
-      expect(response.content_type).to eq("application/json")
+      before(:each) do
+        @business = FactoryGirl.create(:business)
+        get business_path(id: @business.id)
+      end
+
+      it "returns json with successful status (200), when sending existing business id" do
+        expect(response).to be_success
+        expect(response.content_type).to eq("application/json")
+      end
+
+      it 'renders the json response containing all columns from the csv' do
+        get business_path(id: @business.id)
+        column_headers = 'id,uuid,name,address,address2,city,state,zip,country,phone,website,created_at'.split(',')
+        expect(column_headers - JSON.parse(response.body).keys).to be_empty
+      end
     end
 
     it 'returns json with bad request status (400), when sending business id that does not exist' do
+      @business = FactoryGirl.create(:business)
       get business_path(id: @business.id+1)
       expect(response).to be_bad_request
       expect(response.content_type).to eq("application/json")
